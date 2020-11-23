b5b6b6c280e9bb42a3fffb5c7048dd8665a82af8
Dániel Virág <dani.virag99@gmail.com>
2020-11-23 23:01:05 +0100

    Car class

---
+++ IRF_Project_XK5TER/IRF_Project_XK5TER/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRF_Project_XK5TER.Entities
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public double Price { get; set; }
+        public string Body { get; set; }
+        public int Mileage { get; set; }
+        public decimal EngV { get; set; }
+        public string EngType { get; set; }
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string Drive { get; set; }
+    }
+}
